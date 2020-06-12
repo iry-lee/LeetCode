8bd69e7884ac17bb27bd02a42078daafa7fb1206
liyirui <liyirui@buaa.edu.cn>
2020-06-12 22:06:32 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * 建立这个项目的目的是在本地建立起一个辅助在LeetCode上刷题的工具。
+ * 针对于LeetCode上的C++题目，提供一些从文件或者命令行读取数据输入的函数，
+ * 使得使用者稍加组合就可以在本地对所写的程序进行调试。
+ *
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+
+
+
+}
