e7345209958e155924a42af550af5473b245c3aa
iry-lee <28668523+iry-lee@users.noreply.github.com>
2020-07-22 20:16:32 +0800

    完成JZOffer15题

---
+++ my_solution/JZOffer12/solution.cpp
@@ -0,0 +1,8 @@
+// 因为输入不方便构造，这个题直接在网站上做的
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if()
+        int map [board.size()][board[0].size()];
+    }
+};
