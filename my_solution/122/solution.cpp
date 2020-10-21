b9500c035ee8a95d6b4fb6b91bec98712659bae9
iry-lee <28668523+iry-lee@users.noreply.github.com>
2020-10-21 09:10:21 +0800

    更新了 买卖股票解法--自我总结.md

--- my_solution/122/solution.cpp
+++ my_solution/122/solution.cpp
@@ -17,4 +17,22 @@ public:
     }
 };
 
-// 与121题一样，可以在上面代码的基础上，进一步优化空间复杂度，这里就不再赘述了
+// 与121题一样，可以在上面代码的基础上，进一步优化空间复杂度：
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int  ans = 0, len = prices.size();
+        if(len == 0) return ans;
+
+        int dp0 = 0-prices[0];     // 持有股票
+        int dp1 = 0;               // 不持有股票
+        // 持有股票 --卖出股票--> 不持有股票，在卖出股票的过程中，获得收益
+        // 不持有股票 --买入股票--> 持有股票
+        for(int i = 1; i < len; i++){
+            int temp = dp0;
+            dp0 = max(dp0, dp1-prices[i]);
+            dp1 = max(dp1, temp + prices[i]);
+        }
+        return dp1;
+    }
+};
